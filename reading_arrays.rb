6bb24632726dad03b244d1248a0fa08133634379
ananyagupta1114 <ananyagupta1114@gmail.com>
2018-08-08 15:42:05 +0000

    Done.

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -7,26 +7,48 @@ STUDENT_NAMES = [
   "Rihanna"
 ]
 
-def first_student_by_index
+def first_student_by_index 
+  STUDENT_NAMES[0]
   # Write a solution that returns the first student in the array student_names
 end
+puts first_student_by_index
 
 def fourth_student_by_index
+  STUDENT_NAMES[3]
   # Write a solution that returns the first student in the array student_names
 end
+puts fourth_student_by_index
 
 def last_student_by_index
   # Write a solution that returns the first student in the array student_names
+  STUDENT_NAMES[5]
+  # Write a solution that returns the first student in the array student_names
 end
+puts last_student_by_index
+
 
 def first_student_by_method
-  # Write a solution that returns the first student using the built in .first method
+  STUDENT_NAMES.first
+  # Write a solution that returns the first student in the array student_names
 end
+puts first_student_by_index
+  # Write a solution that returns the first student using the built in .first method
 
 def last_student_by_method
   # Write a solution that returns the first student using the built in .last method
+  STUDENT_NAMES.last
+  # Write a solution that returns the first student in the array student_names
 end
+puts last_student_by_index
+
 
 def first_second_and_third_students
-  # Write a solution that returns the first, second and third students
+  STUDENT_NAMES[0..2]
+  # Write a solution that returns the first student in the array student_names
 end
+puts first_second_and_third_students
+# STUDENT_NAMES[3]
+#   # Write a solution that returns the first student in the array student_names
+# end
+# puts first_second_and_third_students_student_by_index
+#   # Write a solution that returns the first, second and third students
